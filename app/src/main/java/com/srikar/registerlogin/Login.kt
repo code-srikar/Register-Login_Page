416fccbc1c2214e787677b1b91d56aafaf092bcd
iam-srikar <srikarmarikanti@gmail.com>
2023-11-20 22:39:47 +0530

    Basic Register and Login Pages in an Application.

--- app/src/main/java/com/srikar/registerlogin/Login.kt
+++ app/src/main/java/com/srikar/registerlogin/Login.kt
@@ -5,47 +5,47 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.text.TextUtils
 import android.widget.Toast
+import com.google.firebase.Firebase
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.auth.auth
 import com.srikar.registerlogin.databinding.ActivityLoginBinding
 import com.srikar.registerlogin.databinding.ActivityMainBinding
 
 class Login : AppCompatActivity() {
 
     private lateinit var binding2 : ActivityLoginBinding
+    private lateinit var auth: FirebaseAuth
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding2 = ActivityLoginBinding.inflate(layoutInflater)
         setContentView(binding2.root)
 
-        val name1 = intent.getStringExtra("uname")
-        val pass1 = intent.getStringExtra("password")
+        // Initialize Firebase Auth
+        auth = Firebase.auth
 
         binding2.logBtn.setOnClickListener(){
-            if(!TextUtils.isEmpty(binding2.name.text.toString()) && !TextUtils.isEmpty(binding2.pass.text.toString())) {
-                if((binding2.name.text.toString() == name1 && binding2.pass.text.toString() == pass1)) {
-                    Toast.makeText(this, "LOGGED IN SUCCESSFULLY", Toast.LENGTH_SHORT).show()
-                    var intent = Intent(this, MainActivity::class.java)
-                    startActivity(intent)
-                    finish()
-                }
-                else{
-                    Toast.makeText(this,"INCORRECT CREDENTIALS",Toast.LENGTH_SHORT).show()
-                }
+            if(!TextUtils.isEmpty(binding2.mail.text.toString()) && !TextUtils.isEmpty(binding2.pass.text.toString())) {
+                auth.signInWithEmailAndPassword(binding2.mail.text.toString(), binding2.pass.text.toString())
+                    .addOnCompleteListener(this) { task ->
+                        if (task.isSuccessful) {
+                            // Sign in success, update UI with the signed-in user's information
+                            val user = auth.currentUser
+                            Toast.makeText(this, "LOGGED IN SUCCESSFULLY", Toast.LENGTH_SHORT).show()
+                            updateUI(user)
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Toast.makeText(
+                                baseContext,
+                                "Authentication failed.",
+                                Toast.LENGTH_SHORT,
+                            ).show()
+                        }
+                    }
             }
             else{
                 Toast.makeText(this,"CREDENTIALS REQUIRED!",Toast.LENGTH_SHORT).show()
-            }
-        }
-
-        binding2.forgot.setOnClickListener(){
-            if(binding2.name.text.toString() == name1) {
-                Toast.makeText(this, "Redirecting...", Toast.LENGTH_SHORT).show()
-                var intent = Intent(this, Forgot::class.java)
-                intent.putExtra("pass1",pass1)
-                startActivity(intent)
-            }
-            else{
-                Toast.makeText(this,"Invalid Username",Toast.LENGTH_SHORT).show()
             }
         }
 
@@ -54,5 +54,11 @@ class Login : AppCompatActivity() {
             startActivity(intent)
         }
 
+    }
+
+    private fun updateUI(user: FirebaseUser?) {
+        var intent = Intent(this, MainActivity::class.java)
+        startActivity(intent)
+        finish()
     }
 }
